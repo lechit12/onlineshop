3e3b55d87d45c40869d0117b7d6f76e0b50c6e37
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2023-12-15 18:01:42 +0100

    addded popup userRegistered

--- onlineshop/src/main/java/pl/onlineshop/onlineshop/controller/HomeController.java
+++ onlineshop/src/main/java/pl/onlineshop/onlineshop/controller/HomeController.java
@@ -1,8 +1,11 @@
 package pl.onlineshop.onlineshop.controller;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.onlineshop.onlineshop.repository.UserRepository;
 
 @Controller
@@ -14,8 +17,10 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home()
+    public String home(RedirectAttributes redirectAttributes)
 {
+    redirectAttributes.addFlashAttribute("message", "Rejestracja udana! Możesz się teraz zalogować.");
+    redirectAttributes.addFlashAttribute("messageType", "success");
     return "index";
 }
 

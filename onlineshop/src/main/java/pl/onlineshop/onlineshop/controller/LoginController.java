ac74ca71da549c77db9c7c69702a2260f0f132c8
Jakub Kleszczynski <kleszczynski.jakub@gmail.com>
2023-12-17 19:32:08 +0100

    addded login.html and sign in form

---
+++ onlineshop/src/main/java/pl/onlineshop/onlineshop/controller/LoginController.java
@@ -0,0 +1,12 @@
+package pl.onlineshop.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String showLogin()
+    {
+        return "login";
+    }
+}
